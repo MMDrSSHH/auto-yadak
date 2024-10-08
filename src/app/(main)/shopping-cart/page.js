b80d707f9900feed96c64bb163443636fddc96ce
MohammadReza Shekohi <mrshekohi079@gmail.com>
2024-10-08 15:54:48 +0330

    feat: added shopping cart details ui and created shopping cart item card file

--- src/app/(main)/shopping-cart/page.js
+++ src/app/(main)/shopping-cart/page.js
@@ -1,7 +1,25 @@
+import Container from "@/components/layout/Container";
 import React from "react";
 
 function ShoppingCartPage() {
-  return <div></div>;
+  return (
+    <Container>
+      {/* Shopping cart */}
+      <div className="mt-[32px] grid grid-cols-12 gap-[20px]">
+        {/* Items list */}
+        <div className="col-span-8 flex flex-col gap-[16px]"></div>
+
+        {/* Cart details */}
+        <div className="col-span-4">
+          <div className="bg-white py-[72px] flex flex-col items-center">
+            <span className="text-[16px] font-medium text-black">جمع سبد خرید شما</span>
+            <span className="mt-[24px] text-black text-[16px] font-bold">1650000تومان</span>
+            <button className="mt-[32px] w-[60%] bg-primary-500 h-[48px] text-white rounded-[8px] text-[14px] font-medium">تایید و تکمیل سفارش</button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 }
 
 export default ShoppingCartPage;
---
+++ src/components/shopping-cart-item-card/ShoppingCartItemCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ShoppingCartItemCard({
+  imageUrl,
+  itemTitle,
+  warranty,
+  price,
+  discountPrice,
+  quantity,
+}) {
+  return <div></div>;
+}
+
+export default ShoppingCartItemCard;
